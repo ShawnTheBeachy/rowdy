e1574314fb2400e2c8d3723cddc4aa68075a2f2f
Shawn Beachy <shawnseanshaun@proton.me>
2026-04-15 11:47:29 -0400

    Add unit tests.

---
+++ test/Rowdy.Tests.Unit/RowdyServerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Rowdy.Tests.Unit;
+
+public sealed class RowdyServerTests
+{
+    [Test]
+    public async Task Request_ShouldReturnResponse_WhenEndpointIsMapped()
+    {
+        // Arrange.
+        var sut = new RowdyServer();
+        sut.MapGet("/", TypedResults.Ok);
+
+        // Act.
+        var client = sut.CreateClient();
+        var response = await client.GetAsync("/");
+
+        // Assert.
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+    }
+
+    [Test]
+    public async Task Request_ShouldReturnResponse_WhenUsingManuallyCreatedClient()
+    {
+        // Arrange.
+        var sut = new RowdyServer();
+        sut.MapGet("/", TypedResults.Ok);
+        var client = new HttpClient { BaseAddress = new Uri(sut.Url) };
+
+        // Act.
+        var response = await client.GetAsync("/");
+
+        // Assert.
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+    }
+}
