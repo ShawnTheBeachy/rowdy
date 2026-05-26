d53c76fd7799c7ccaabc7d99b3d80fc93a130fa0
Shawn Beachy <shawnseanshaun@proton.me>
2026-05-26 11:23:40 -0400

    Add header filters.

--- src/Rowdy/EndpointConventionBuilderExtensions.cs
+++ src/Rowdy/EndpointConventionBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace Rowdy;
 
@@ -8,6 +9,46 @@ namespace Rowdy;
 /// </summary>
 public static class EndpointConventionBuilderExtensions
 {
+    /// <summary>
+    /// Returns 404 if the specified header does not match.
+    /// </summary>
+    public static T WithHeaderFilter<T>(
+        this T builder,
+        string headerName,
+        string? headerValue,
+        StringComparison comparison = StringComparison.Ordinal
+    )
+        where T : IEndpointConventionBuilder =>
+        builder.WithHeaderFilter(
+            headerName,
+            x =>
+                x == StringValues.Empty
+                    ? string.IsNullOrEmpty(headerValue)
+                    : string.Equals(headerValue, x.ToString(), comparison)
+        );
+
+    /// <summary>
+    /// Returns 404 if the specified header does not match.
+    /// </summary>
+    public static T WithHeaderFilter<T>(
+        this T builder,
+        string headerName,
+        Func<StringValues, bool> match
+    )
+        where T : IEndpointConventionBuilder
+    {
+        builder.AddEndpointFilter(
+            async (ctx, next) =>
+            {
+                if (match(ctx.HttpContext.Request.Headers[headerName]))
+                    return await next(ctx);
+
+                return TypedResults.NotFound();
+            }
+        );
+        return builder;
+    }
+
     /// <summary>
     /// Returns 404 if the specified query string value does not exist or does not match.
     /// </summary>
